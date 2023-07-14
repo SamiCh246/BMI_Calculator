3856d37c874e8692285ef7958ea680f4df434084
Sami Cheema <samicheema246@gmail.com>
2023-07-13 21:54:55 -0400

    first commit

---
+++ BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI_Calculator
+//
+//  Created by Sami Sajjad Cheema on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
